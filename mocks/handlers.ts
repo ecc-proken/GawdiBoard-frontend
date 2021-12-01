2fc2f0416a14b2184f0f468db2bfcb6608adc689
shibayaman <shibayaman7374@gmail.com>
2021-12-02 01:54:45 +0900

    Add jsonClientのテスト

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -9,4 +9,20 @@ export const handlers = [
       })
     );
   }),
+  rest.get('/success', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        message: 'hello world',
+      })
+    );
+  }),
+  rest.get('/error', (req, res, ctx) => {
+    return res(
+      ctx.status(400),
+      ctx.json({
+        message: 'Error!',
+      })
+    );
+  }),
 ];
---
+++ utils/__tests__/httpClient.test.ts
@@ -0,0 +1,54 @@
+import { jsonClient } from '../httpClient';
+import { server } from '../../mocks/server';
+import { rest } from 'msw';
+
+describe('jsonClient', () => {
+  it('APIを呼べる', async () => {
+    const data = await jsonClient('/success');
+    expect(data.message).toBe('hello world');
+  });
+
+  it('200番台じゃないとExceptionを投げる', async () => {
+    await expect(jsonClient('/error')).rejects.toThrow('Error!');
+  });
+
+  it('クエリパラメタがつく。配列はa[]=1&a[]=2...の形になる', async () => {
+    server.use(
+      rest.get('/success', async (req, res, ctx) => {
+        const queryParameter = req.url.searchParams.toString();
+        return res(ctx.status(200), ctx.json({ queryParameter }));
+      })
+    );
+
+    await expect(
+      jsonClient('/success', { params: { a: 'hello', b: 'world' } })
+    ).resolves.toHaveProperty('queryParameter', 'a=hello&b=world');
+
+    await expect(
+      jsonClient('/success', { params: { a: ['1', '2'], b: ['3', '4'] } })
+    ).resolves.toHaveProperty(
+      'queryParameter',
+      encodeURI('a[]=1&a[]=2&b[]=3&b[]=4')
+    );
+  });
+
+  it('リクエストボディがついてContent-Typeがapplication/jsonになる', async () => {
+    server.use(
+      rest.post('/success', async (req, res, ctx) => {
+        const requestBody =
+          typeof req.body === 'string' ? req.body : JSON.stringify(req.body);
+        const contentType = req.headers.get('Content-Type');
+        return res(ctx.status(200), ctx.json({ requestBody, contentType }));
+      })
+    );
+
+    const body = { a: 'hello world', b: [2, 'b'], c: { d: 'nested' } };
+    await expect(
+      jsonClient('/success', { body, method: 'POST' })
+    ).resolves.toHaveProperty('requestBody', JSON.stringify(body));
+
+    await expect(
+      jsonClient('/success', { body: { a: 1 }, method: 'POST' })
+    ).resolves.toHaveProperty('contentType', 'application/json');
+  });
+});
