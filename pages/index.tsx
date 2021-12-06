a406f5d3cb0c0d964a793bbdd244f2828325a505
shibayaman <shibayaman7374@gmail.com>
2021-12-06 11:26:13 +0900

    Add react-hook-formとyup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,45 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
+import * as yup from 'yup';
 import { jsonClient } from '../utils/httpClient';
 
+type FormValue = {
+  keyword: string;
+};
+
+const schema = yup
+  .object({
+    keyword: yup.string().required('ここは絶対に入力してね'),
+  })
+  .required();
+
 const Home: NextPage = () => {
-  const { data, error, isLoading } = useQuery('test', () =>
-    jsonClient('/success', {
-      params: { a: 'hello', b: ['1', '2', '3'] },
-    })
+  const [keyword, setKeyword] = useState('');
+  const { data, error, isLoading } = useQuery(
+    'test',
+    () =>
+      jsonClient('/success', {
+        params: { keyword },
+      }),
+    {
+      enabled: keyword.length !== 0,
+    }
   );
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValue>({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = handleSubmit(({ keyword }) => {
+    setKeyword(keyword);
+  });
 
   return (
     <div>
@@ -18,6 +49,11 @@ const Home: NextPage = () => {
       </Head>
       <main>
         <h1>Gawdi Boardへようこそ</h1>
+        <form onSubmit={onSubmit}>
+          <input {...register('keyword')}></input>
+          {errors.keyword && <span>{errors.keyword.message}</span>}
+          <button>押す</button>
+        </form>
         {isLoading && <p>ロード中だよ...</p>}
         {data && <p>{data.message}</p>}
         {error && (
