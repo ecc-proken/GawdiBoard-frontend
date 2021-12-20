a6d7a0b09c7349461af5a7b99b60f130360372ab
shibayaman <shibayaman7374@gmail.com>
2021-12-20 15:57:34 +0900

    Add useInfiniteOffersのテスト

---
+++ hooks/requests/__tests__/offers.test.tsx
@@ -0,0 +1,86 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { useInfiniteOffers } from '../offers';
+
+const fakeQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+// パラメタがちゃんとつくかとかはフックを呼び出すコンポーネント側で十分にテストできると思うのでここには書かない。
+describe('useInfiniteOffers', () => {
+  it('pageを渡さないと1から始まる', async () => {
+    const { result, waitFor } = renderHook(() => useInfiniteOffers(), {
+      wrapper: ({ children }) => (
+        <QueryClientProvider client={fakeQueryClient()}>
+          {children}
+        </QueryClientProvider>
+      ),
+    });
+
+    await waitFor(() => result.current.isSuccess);
+
+    const data = result.current.data?.pages[0].meta.current_page;
+    expect(data).toBe(1);
+  });
+
+  it('pageを渡すとその数字から始まる', async () => {
+    const { result, waitFor } = renderHook(
+      () => useInfiniteOffers({ page: '2' }),
+      {
+        wrapper: ({ children }) => (
+          <QueryClientProvider client={fakeQueryClient()}>
+            {children}
+          </QueryClientProvider>
+        ),
+      }
+    );
+
+    await waitFor(() => result.current.isSuccess);
+
+    const data = result.current.data?.pages[0].meta.current_page;
+    expect(data).toBe(2);
+  });
+
+  it('fetchNextPageでpageが1進む', async () => {
+    const { result, waitFor } = renderHook(
+      () => useInfiniteOffers({ page: '1' }),
+      {
+        wrapper: ({ children }) => (
+          <QueryClientProvider client={fakeQueryClient()}>
+            {children}
+          </QueryClientProvider>
+        ),
+      }
+    );
+
+    await waitFor(() => result.current.isSuccess);
+    expect(result.current.hasNextPage).toBe(true);
+
+    result.current.fetchNextPage();
+    await waitFor(() => result.current.isFetching);
+    await waitFor(() => !result.current.isFetching);
+    expect(result.current.data?.pages[1].meta.current_page).toBe(2);
+  });
+
+  it('最後のページになったらhasNextPageがfalseになる', async () => {
+    const { result, waitFor } = renderHook(
+      () => useInfiniteOffers({ page: '3' }),
+      {
+        wrapper: ({ children }) => (
+          <QueryClientProvider client={fakeQueryClient()}>
+            {children}
+          </QueryClientProvider>
+        ),
+      }
+    );
+
+    await waitFor(() => result.current.isSuccess);
+
+    expect(result.current.hasNextPage).toBe(false);
+  });
+});
