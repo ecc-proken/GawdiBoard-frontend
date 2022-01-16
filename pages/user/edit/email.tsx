6ecdd7f0a82fa1b01ddd366eb632a685cefbbd1f
shibayaman <shibayaman7374@gmail.com>
2022-01-17 06:42:29 +0900

    Add メール認証を促すモーダル

--- pages/user/edit/email.tsx
+++ pages/user/edit/email.tsx
@@ -1,23 +1,32 @@
-import Router from 'next/router';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import type { ReactElement } from 'react';
+import Modal from 'react-modal';
 import Layout from '../../../components/layouts/Layout';
 import { useEmailForm } from '../../../hooks/forms/useEmailForm';
 import { user } from '../../../mocks/handlers/auth';
 
 function EditEmailPage() {
+  const [showModal, setShowModal] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors },
+    getValues,
   } = useEmailForm();
 
-  const onSubmit = handleSubmit((data) => {
-    console.log('submitting with ', data);
+  const router = useRouter();
+
+  const onSubmit = handleSubmit(() => {
+    setShowModal(true);
   });
   const handleCancel = () => {
     if (confirm('入力内容を破棄しますか?')) {
-      Router.push(`/user/${user.student_number}`);
+      router.push(`/user/${user.student_number}`);
     }
+  };
+  const handleDone = () => {
+    router.push(`/user/${user.student_number}`);
   };
 
   return (
@@ -39,6 +48,14 @@ function EditEmailPage() {
           </form>
         </>
       )}
+      <Modal isOpen={showModal}>
+        <h3>新しいメールアドレスの認証が必要です</h3>
+        <p className="modal-message">
+          <span className="new-email">{getValues('email')}</span>
+          に認証メールを送りました。送られたメールに従ってメールアドレスを認証してください。
+        </p>
+        <button onClick={handleDone}>閉じる</button>
+      </Modal>
       <style jsx>{`
         form {
           margin: 8px;
@@ -49,6 +66,14 @@ function EditEmailPage() {
         }
         label {
           margin-right: 8px;
+        }
+        .modal-message {
+          margin: 8px;
+        }
+        .new-email {
+          font-size: 1.2rem;
+          padding-inline: 4px;
+          color: tomato;
         }
       `}</style>
     </div>
